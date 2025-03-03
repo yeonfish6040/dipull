e101ca5339ddc27da6d4332edaf48aeb63364cbc
Yeonjun Lee <72007187+yeonfish6040@users.noreply.github.com>
2025-03-04 07:26:21 +0900

    tmp: stay apply range (#410)

--- dipull/src/utils/date.ts
+++ dipull/src/utils/date.ts
@@ -16,9 +16,9 @@ const stayApplyTimes = async (): Promise<{
   [key: string]: { day: number; hour: number };
 }> => {
   return {
-    1: { day: 1, hour: 22 },
-    2: { day: 1, hour: 22 },
-    3: { day: 1, hour: 22 },
+    1: { day: 2, hour: 22 },
+    2: { day: 2, hour: 22 },
+    3: { day: 2, hour: 22 },
   } as {
     [key: string]: { day: number; hour: number };
   };
